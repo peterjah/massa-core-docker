3cf9b8a734cee72160fdd8d8e6431608802c68f3
Pierre Seznec <petterseznec@gmail.com>
2024-01-03 23:48:11 +0100

    version 28.2

--- docker-build.sh
+++ docker-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # This builds the image  for all architectures an publish it on dockerhub
-#DOCKER_BUILDKIT=1 docker buildx build --platform linux/amd64,linux/arm/v7,linux/arm64/v8 -t peterjah/massa-core --push --build-arg VERSION=TEST.27.6 .
+#DOCKER_BUILDKIT=1 docker buildx build --platform linux/amd64,linux/arm/v7,linux/arm64/v8 -t peterjah/massa-core --push --build-arg VERSION=DEVN.28.2 .
 # This builds the image for amd64 an load it in local docker
-DOCKER_BUILDKIT=1 docker buildx build --platform linux/amd64 -t peterjah/massa-core --load --build-arg VERSION=TEST.27.6  .
+DOCKER_BUILDKIT=1 docker buildx build --platform linux/amd64 -t peterjah/massa-core --load --build-arg VERSION=DEVN.28.2  .
