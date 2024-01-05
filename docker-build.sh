b7ec961eed04144e5a815934b973ea70177cbecf
Pierre Seznec <petterseznec@gmail.com>
2024-01-05 19:03:19 +0100

    MAIN.1.0 🎉

--- docker-build.sh
+++ docker-build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-# This builds the image  for all architectures an publish it on dockerhub
-#DOCKER_BUILDKIT=1 docker buildx build --platform linux/amd64,linux/arm/v7,linux/arm64/v8 -t peterjah/massa-core --push --build-arg VERSION=DEVN.28.2 .
-# This builds the image for amd64 an load it in local docker
-DOCKER_BUILDKIT=1 docker buildx build --platform linux/amd64 -t peterjah/massa-core --load --build-arg VERSION=DEVN.28.2  .
+# This builds the image an load it locally. you can build for different architectures with --platform option like linux/amd64,linux/arm/v7,linux/arm64/v8
+DOCKER_BUILDKIT=1 docker buildx build --progress=plain --no-cache --platform linux/amd64 -t peterjah/massa-core --load --build-arg VERSION=MAIN.1.0  .
